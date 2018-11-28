f680dd13064e36d3ab1809766c4dca90d3453b49
Chainfailure <chainfailure@protonmail.com>
2018-11-28 02:49:01 +0100

    hash serialized to hex when marshaling to JSON

--- crypto/hash.go
+++ crypto/hash.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"encoding/hex"
+	"encoding/json"
 )
 
 // Simple wrapper for byte arrays so I can easily write it to strings in an
@@ -10,4 +11,13 @@ type Hash []byte
 
 func (h Hash) String() string {
 	return hex.EncodeToString(h)
+}
+
+func (h Hash) MarshalJSON() ([]byte, error) {
+	data, err := json.Marshal(h.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
