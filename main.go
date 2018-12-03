e4cd5cd885183173b22cab0294d412d58f403296
Chainfailure <chainfailure@protonmail.com>
2018-12-03 18:52:52 +0100

    set up database connection as well as migrate the buckets

--- main.go
+++ main.go
@@ -21,35 +21,28 @@ func main() {
 	}
 	defer storage.CloseDatabase()
 
-	err = storage.Setup()
+	err = storage.Migrate()
 	if err != nil {
 		panic(err)
 	}
 
 	parameters.Set(parameters.Main)
-
 	mainchain := blockchain.NewBlockchain()
 
 	coinbase, err = transaction.NewCoinbase("rsyBe3AcPF61VFMi48phGcfsLyvho4mr", 2000, time.Now())
 	if err != nil {
 		panic(err)
 	}
-
 	genesisBody := block.NewBody()
 	err = genesisBody.AddTransaction(coinbase)
 	if err != nil {
 		panic(err)
 	}
-
 	genesisBlock := block.NewGenesisBlock(time.Now(), genesisBody)
 	err = mainchain.AddBlock(genesisBlock)
 	if err != nil {
 		panic(err)
 	}
-
-	storage.BlockRepository.Store(genesisBlock)
-
-	storage.BlockRepository.Get(genesisBlock.Header.Hash)
 
 	body := block.NewBody()
 	coinbase, err = transaction.NewCoinbase("rsyBe3AcPF61VFMi48phGcfsLyvho4mr", 1500, time.Now())
