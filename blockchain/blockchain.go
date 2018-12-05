e1c770660110b81f32dd598efc55e34870b90194
Chainfailure <chainfailure@protonmail.com>
2018-12-05 06:15:55 +0100

    moved block validation to ValidateBlock method on blockchain struct so I can validate without directly adding the block

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -31,6 +31,7 @@ func (b *Blockchain) HasGenesis() bool {
 // and setting the tip to the appropriate state.
 func (b *Blockchain) Hydrate() error {
 	storage.WalkBlocks(func(currentBlock *block.Block) error {
+
 		if b.Tip == nil || currentBlock.Header.Height > b.Tip.Header.Height {
 			b.Tip = currentBlock
 		}
@@ -43,27 +44,6 @@ func (b *Blockchain) Hydrate() error {
 
 // Adds a block to the chain if it passes some validation.
 func (b *Blockchain) AddBlock(currentBlock *block.Block) error {
-	// ensure the prevhash this block is referring to is actually the tip
-	// with exception of ofcourse the genesis block.
-	prevHash := hex.EncodeToString(currentBlock.Header.PrevHash)
-	if currentBlock.Header.Height != 0 &&
-		hex.EncodeToString(b.Tip.Header.Hash) != prevHash {
-		return fmt.Errorf(
-			"block has parent that isn't the current tip block: %v",
-			currentBlock.Header.Hash)
-	}
-
-	// ensure the height is that of the previous block +1 with the obvious
-	// exception of the genesis block
-	if b.Tip != nil && currentBlock.Header.Height != (b.Tip.Header.Height+1) {
-		return fmt.Errorf(
-			"block %v being added has an unexpected height. expected %v, got %v",
-			currentBlock.Header.Hash,
-			b.Tip.Header.Height+1,
-			currentBlock.Header.Height)
-	}
-
-	// Validate the block itself
 	if err := b.ValidateBlock(currentBlock); err != nil {
 		return err
 	}
@@ -81,6 +61,25 @@ func (b *Blockchain) AddBlock(currentBlock *block.Block) error {
 // Validates the to-be-added block, currently only checks the validity of the
 // PoW.
 func (b *Blockchain) ValidateBlock(currentBlock *block.Block) error {
+	// ensure the prevhash this block is referring to is actually the tip
+	// with exception of the genesis block.
+	prevHash := hex.EncodeToString(currentBlock.Header.PrevHash)
+	if currentBlock.Header.Height != 0 &&
+		hex.EncodeToString(b.Tip.Header.Hash) != prevHash {
+		return fmt.Errorf(
+			"block has parent that isn't the current tip block: %v",
+			currentBlock.Header.Hash)
+	}
+
+	// ensure the height is that of the previous block +1 with the exception of the genesis block.
+	if b.Tip != nil && currentBlock.Header.Height != (b.Tip.Header.Height+1) {
+		return fmt.Errorf(
+			"block %v being added has an unexpected height. expected %v, got %v",
+			currentBlock.Header.Hash,
+			b.Tip.Header.Height+1,
+			currentBlock.Header.Height)
+	}
+
 	// check if PoW checks out
 	powValid, err := consensus.ValidatePOW(currentBlock.Header, parameters.Current().MinimumPoW)
 	if err != nil {
